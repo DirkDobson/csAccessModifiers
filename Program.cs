58cf0625575803b1faf094af3f22ae04c12276be
Dirk Dobson <dirkvancouver@live.com>
2019-01-29 17:51:31 -0700

    private object

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace accessModifiers
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            System.Console.WriteLine(person.GetBirthdate());
         }
     }
 }
